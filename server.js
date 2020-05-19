c84ff3d89ff8222e132192b6fcaeca3280859e16
Sean Tracey <seanmtracey@gmail.com>
2020-05-19 01:15:01 +0100

    Enable dynamic allocation of port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const port = 3000
+const port = process.env.PORT || 3000;
 const app = express()
 const morgan = require('morgan')
 

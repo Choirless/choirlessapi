9ad8bc14a6343895a7f95a7bb4f8da04bec9a999
Glynn Bird <glynn.bird@gmail.com>
2020-05-21 10:19:08 +0100

    add simple key cache, to prevent every API call making an extra call to Cloudant

--- checkAPIKey.js
+++ checkAPIKey.js
@@ -2,9 +2,20 @@ const Nano = require('nano')
 const nano = Nano(process.env.COUCH_URL)
 const db = nano.db.use(process.env.COUCH_KEYS_DATABASE)
 
+// cache of API keys
+const keyCache = {
+
+}
+
 async function checkAPIKey (req, res, next) {
   if (req.query.apikey) {
-    const storedKey = await db.get(req.query.apikey)
+    // if we've seen this key before, use the cached key
+    let storedKey = keyCache[req.query.apikey]
+    if (!storedKey) {
+      // otherwise fetch from database
+      storedKey = await db.get(req.query.apikey)
+      keyCache[req.query.apikey] = storedKey
+    }
 
     if (storedKey) {
       next()
